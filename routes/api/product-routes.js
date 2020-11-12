e30a7cd8e5dfa4764802d3af06f367246b54a82c
Jenniffer <jenlpac@yahoo.com>
2020-11-12 12:25:26 -0600

    complete Product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -130,6 +130,16 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProduct => res.json(dbProduct))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
