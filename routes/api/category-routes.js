6aecaf941da8f61f76f7806a4ecfabca01abebb2
Jenniffer <jenlpac@yahoo.com>
2020-11-11 23:33:43 -0600

    create Category GET routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,12 +5,46 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    // be sure to include its associated Products
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name','price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(dbCategory => {
+    res.json(dbCategory);
+  })
+  
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name','price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(dbCategory => {
+    res.json(dbCategory);
+  })
 });
 
 router.post('/', (req, res) => {
